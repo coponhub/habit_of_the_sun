a1320778dd4a6640fbb0a02f18fc5714559f5493
Kensuke Aoyagi <coponhtc@gmail.com>
2017-12-13 14:30:37 +0000

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -20,23 +20,29 @@ def flatrepeat(iterator,n):
 
 
 MAX_PWM_DUTY = 1000000
-interval = 0.001
-step = 0.0001
+interval = 0.01
+step = 0.001
 PORT = 18
 FREQ = 2000
-GRAD = 1
+GRAD = 3
+TIME_GRAD = 1.01
 MAX_LUM=1000000
-MIN_LUM=92000
+MIN_LUM=130000
+LIFTER_LUM_DIFF = 10000
+MIN_LUM_LIFT=30000
+MIN_LUM_CEIL = MIN_LUM + MIN_LUM_LIFT
 
 def pos(x):
     return max(0,x)
 def around(x):
     return abs(round(x))
 def sine(rad):
-    return min(MAX_LUM, around((math.cos(rad/GRAD) + 1.203) * (1/1.1) * MAX_PWM_DUTY) // 2)
+    return (math.cos(rad) + 1)**GRAD / 2**GRAD
+def wave(rad):
+    return min(MAX_LUM, around(sine(rad) * (MAX_LUM - MIN_LUM) + MIN_LUM))
 
 def rescale(value):
-    return pos(100 - math.log(value - MIN_LUM + 1, 1.12))
+    return pos(1000 - math.log(value - MIN_LUM + 1, TIME_GRAD))
 def lazy(value):
     time.sleep(interval * rescale(value))
 
@@ -48,14 +54,15 @@ def change_lum(lightness):
 
 atexit.register(change_lum, 0)
 
-curve = (sine(x) for x in frange(-math.pi*GRAD, math.pi*GRAD, step))
+curve = (wave(x) for x in frange(-math.pi, math.pi, step))
 #curve3 = flatrepeat(curve,3)
 curveE = itertools.cycle(curve)
 
 for i,v in enumerate(curveE):
-    if i % 200 == 0:
+    if i % 100 == 0:
         t = time.strftime("%H:%M:%S", time.localtime())
         print(v, t)
-    lazy(v)
+    #lazy(v)
+    time.sleep(interval)
     change_lum(v)
 
